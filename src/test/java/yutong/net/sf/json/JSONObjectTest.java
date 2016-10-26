9f62d49a1a7707d3954756cf5bd72a41064fe58f
yuxiangtong <545285858@qq.com>
2016-10-26 17:11:28 +0800

    add map test

--- src/test/java/yutong/net/sf/json/JSONObjectTest.java
+++ src/test/java/yutong/net/sf/json/JSONObjectTest.java
@@ -1,6 +1,8 @@
 package yutong.net.sf.json;
 
 import static org.junit.Assert.assertEquals;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.junit.Test;
 import net.sf.json.JSONObject;
 
@@ -25,6 +27,20 @@ public class JSONObjectTest {
 
         String expected = "{\"key0\":\"value0\",\"key3\":\"value3\"}";
         assertEquals(expected, jsonString);
+
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("key0", "");
+        map.put("key2", null);
+        map.put("key3", "value3");
+        map.put("key0", "value0");
+
+        Map<String, String> parMap = new LinkedHashMap<String, String>();
+        for (int i = 0; i < 10; i++) {
+            //parMap.put("{" + (i) + "}", "参数值"+i);
+        }
+
+        map.put("key1", parMap);
+        System.out.println(JSONObject.fromObject(map).toString());
 
     }
 }
